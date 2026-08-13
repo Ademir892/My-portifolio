c556d1935d1d1b6cb4c12e5ad18b23f63abd1330
Ademir Marmitt Traesel <ademirtraesel@hotmail.com>
2026-08-13 16:02:33 -0300

    refactor: improve accessibility and clean project structure

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,18 +1,25 @@
 import { Outlet } from "react-router-dom";
 
-import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import ScrollToTop from "../../components/ScrollToTop";
 
 export default function MainLayout() {
   return (
-    <>
+    <div id="top">
+      <ScrollToTop />
+
+      <a href="#main-content" className="skip-link">
+        Pular para o conteúdo
+      </a>
+
       <Header />
 
-      <main>
+      <main id="main-content" tabIndex={-1}>
         <Outlet />
       </main>
 
       <Footer />
-    </>
+    </div>
   );
 }
--- src/utils/exportConst.ts
+++
@@ -1,4 +0,0 @@
-  export const userPart = "contato";
-  export const domainPart = "ademir_dev_mentorphysical.com";
-  export const finalEmailDisplay = `${userPart}@${domainPart}`;
-  export const actualEmailForMailto = "ademirtraesel@hotmail.com"; 
